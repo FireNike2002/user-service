bbbccf8b1f0d7637ad2afc4c65abc03a2fee944f
Нуар <nmikehin@gmail.com>
2026-04-11 00:27:53 +0400

    V5

--- src/main/java/com/example/userservice/dao/UserDao.java
+++ src/main/java/com/example/userservice/dao/UserDao.java
@@ -1,62 +1,62 @@
-package com.example.userservice.dao;
-
-import com.example.userservice.model.User;
-import com.example.userservice.util.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.HibernateException;
-import java.util.List;
-
-
-public class UserDao {
-
-    private final SessionFactory sessionFactory;
-
-    public UserDao(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-
-    public void create(User user) {
-        Transaction tx = null;
-        try (Session session = sessionFactory.openSession()) {
-            tx = session.beginTransaction();
-            session.persist(user);
-            tx.commit();
-        } catch (HibernateException e) {
-            if (tx != null) tx.rollback();
-            throw e;
-        }
-    }
-
-    public User read(Long id) {
-        try (Session session = sessionFactory.openSession()) {
-            return session.get(User.class, id);
-        }
-    }
-
-    public List<User> readAll() {
-        try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("from User", User.class).list();
-        }
-    }
-
-    public void update(User user) {
-        Transaction tx = null;
-        try (Session session = sessionFactory.openSession()) {
-            tx = session.beginTransaction();
-            session.merge(user);
-            tx.commit();
-        }
-    }
-
-    public void delete(Long id) {
-        Transaction tx = null;
-        try (Session session = sessionFactory.openSession()) {
-            tx = session.beginTransaction();
-            User user = session.get(User.class, id);
-            if (user != null) session.remove(user);
-            tx.commit();
-        }
-    }
-}
+//package com.example.userservice.dao;
+//
+//import com.example.userservice.model.User;
+//import com.example.userservice.util.HibernateUtil;
+//import org.hibernate.Session;
+//import org.hibernate.SessionFactory;
+//import org.hibernate.Transaction;
+//import org.hibernate.HibernateException;
+//import java.util.List;
+//
+//
+//public class UserDao {
+//
+//    private final SessionFactory sessionFactory;
+//
+//    public UserDao(SessionFactory sessionFactory) {
+//        this.sessionFactory = sessionFactory;
+//    }
+//
+//    public void create(User user) {
+//        Transaction tx = null;
+//        try (Session session = sessionFactory.openSession()) {
+//            tx = session.beginTransaction();
+//            session.persist(user);
+//            tx.commit();
+//        } catch (HibernateException e) {
+//            if (tx != null) tx.rollback();
+//            throw e;
+//        }
+//    }
+//
+//    public User read(Long id) {
+//        try (Session session = sessionFactory.openSession()) {
+//            return session.get(User.class, id);
+//        }
+//    }
+//
+//    public List<User> readAll() {
+//        try (Session session = sessionFactory.openSession()) {
+//            return session.createQuery("from User", User.class).list();
+//        }
+//    }
+//
+//    public void update(User user) {
+//        Transaction tx = null;
+//        try (Session session = sessionFactory.openSession()) {
+//            tx = session.beginTransaction();
+//            session.merge(user);
+//            tx.commit();
+//        }
+//    }
+//
+//    public void delete(Long id) {
+//        Transaction tx = null;
+//        try (Session session = sessionFactory.openSession()) {
+//            tx = session.beginTransaction();
+//            User user = session.get(User.class, id);
+//            if (user != null) session.remove(user);
+//            tx.commit();
+//        }
+//    }
+//}
